3f867f619870310257b1cfa00f36c0999dbd54ce
cruel32 <cshee8508@gmail.com>
2019-08-29 21:57:25 +0900

    flutter study

--- flutters/lib/main.dart
+++ flutters/lib/main.dart
@@ -1,28 +1,74 @@
 import 'package:flutter/material.dart';
-import 'package:flutters/blocprovs/example-bloc-prov.dart';
-import 'package:flutters/blocs/example-bloc.dart';
 import 'package:flutters/theme/style.dart';
 import 'package:flutters/screens/example1/examplescreen1.dart';
 import 'package:flutters/screens/example2/examplescreen2.dart';
 
-
-void main() {
-  runApp(ExampleApp());
+void main(){
+  runApp(NacaApp());
 }
-class ExampleApp extends StatelessWidget {
+
+class NacaApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return ExampleProvider(
-      bloc: ExampleBloc(),
-      child: MaterialApp(
-        title: 'ExampleApp',
-        theme: appTheme(),
-        initialRoute: '/',
-        routes: <String, WidgetBuilder>{
-          "/": (BuildContext context) => ExScreen1(),
-          "/ExScreen2": (BuildContext context) => ExScreen2(),
-        },
+  Widget build(BuildContext context){
+    return Material(
+      // 열(Column)은 수직 선형 레이아웃임 
+      child: Column (
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Text('Hello, world!', textDirection: TextDirection.ltr),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+// return Scaffold(
+//   bottomNavigationBar: BottomNavigationBar(
+//     items: const <BottomNavigationBarItem>[
+//       BottomNavigationBarItem(
+//         icon: Icon(Icons.home),
+//         title: Text('Home'),
+//       ),
+//       BottomNavigationBarItem(
+//         icon: Icon(Icons.home),
+//         title: Text('Home'),
+//       ),
+//       BottomNavigationBarItem(
+//         icon: Icon(Icons.home),
+//         title: Text('Home'),
+//       ),
+//     ]
+//   ),
+// );
+// }
+
+// import 'package:flutters/blocprovs/example-bloc-prov.dart';
+// import 'package:flutters/blocs/example-bloc.dart';
+// import 'package:flutters/theme/style.dart';
+// import 'package:flutters/screens/example1/examplescreen1.dart';
+// import 'package:flutters/screens/example2/examplescreen2.dart';
+
+
+// void main() {
+//   runApp(ExampleApp());
+// }
+// class ExampleApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ExampleProvider(
+//       bloc: ExampleBloc(),
+//       child: MaterialApp(
+//         title: 'ExampleApp',
+//         theme: appTheme(),
+//         initialRoute: '/',
+//         routes: <String, WidgetBuilder>{
+//           "/": (BuildContext context) => ExScreen1(),
+//           "/ExScreen2": (BuildContext context) => ExScreen2(),
+//         },
+//       ),
+//     );
+//   }
+// }
--- flutters/lib/screens/example1/components/body.dart
+++ flutters/lib/screens/example1/components/body.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutters/screens/example2/examplescreen2.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: RaisedButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: Text('Go back!'),
+      child: Column(
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Go back!'),
+          ),
+          RaisedButton(
+            onPressed: () {
+              print('hi');
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ExScreen2()),
+              );
+
+            },
+            child: Text('hi'),
+          )
+        ],
       ),
     );
   }
