80fa10d1eeb13715bacadb8cbbabbf4d16e5e2f5
cruel32 <cshee8508@gmail.com>
2019-08-31 18:21:21 +0900

    param 테스트

--- flutters/lib/screens/main/SecondPage.dart
+++ flutters/lib/screens/main/SecondPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutters/screens/example2/examplescreen2.dart';
 
 class SecondPage extends StatelessWidget {
   const SecondPage({Key key}) : super(key: key);
@@ -7,7 +8,31 @@ class SecondPage extends StatelessWidget {
   Widget build(BuildContext context){
     return Scaffold(
       body: Center(
-        child: Text('SecondPage'),
+        child: Column(
+          children: <Widget>[
+            Text('SecondPage'),
+            RaisedButton(
+                child: Text('route test', style: TextStyle(fontSize: 24)),
+                onPressed: (){
+                  Navigator.of(context).push(
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) => ExScreen2(),
+                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                        var begin = Offset(1.0, 0);
+                        var end = Offset.zero;
+                        var tween = Tween(begin: begin, end: end);
+                        var offsetAnimation = animation.drive(tween);
+                        return SlideTransition(
+                          position: offsetAnimation,
+                          child: child,
+                        );
+                      },
+                    )
+                  );
+                },
+            ),
+          ],
+        )
       )
     );
   }
