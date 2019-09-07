33b9f25d12f8aab3da14e52dad75cef5c5882b55
cruel-32 <cruel32@nate.com>
2019-09-07 13:35:43 +0900

    flutter layout 구성중...

--- flutters/lib/MainScreen/HomeScreen/index.dart
+++ flutters/lib/MainScreen/HomeScreen/index.dart
@@ -10,9 +10,9 @@ import '../../provider/mainProv.dart';
 
 class HomeScreen extends StatelessWidget {
   final List<LinkItem> linkItems = <LinkItem>[
-    LinkItem(title:'이벤트관리', description:'이벤트 생성,삭제,출석관리', icon:'', index:1),
-    LinkItem(title:'회원관리', description:'회원 신규,수정,삭제관리', icon:'', index:2),
-    LinkItem(title:'통계보기', description:'여러가지 통계 보기', icon:'', index:3),
+    LinkItem(title:'이벤트관리', description:'이벤트 생성,삭제,출석관리', icon:Icons.calendar_today, index:1),
+    LinkItem(title:'회원관리', description:'회원 신규,수정,삭제관리', icon:Icons.people, index:2),
+    LinkItem(title:'통계보기', description:'여러가지 통계 보기', icon:Icons.insert_chart, index:3),
   ];
 
   HomeScreen({Key key}) : super(key: key);
@@ -21,22 +21,42 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     final mainIndex = Provider.of<MainIndex>(context);
-
-    return Scaffold(
-      body: Center(
-        child: ListView.builder(
+    return Center(
+      child:Container(
+        width: 200,
+        child:ListView.builder(
+          shrinkWrap: true,
           itemCount: linkItems.length,
           itemBuilder: (BuildContext context, int index){
-            return ListTile(
-              title: Text(linkItems[index].title),
-              onTap: () {
-                print('index: $index random: ${Random().nextInt(10)}  context: $context');
-                mainIndex.setIndex(linkItems[index].index);
-              },
+            return Container(
+              // color: Colors.red,
+              padding: const EdgeInsets.symmetric(vertical: 40),
+              child: GestureDetector(
+                onTap: () {
+                  print('index: $index random: ${Random().nextInt(10)}  context: $context');
+                  mainIndex.setIndex(linkItems[index].index);
+                },
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      linkItems[index].icon,
+                      color: Color(0xFF707070),
+                      size: 28.0,
+                    ),
+                    Text(
+                      linkItems[index].title,
+                      style: TextStyle(
+                        fontSize:28.0,
+                        color: Color(0xFF707070),
+                      ),
+                    ),
+                  ],
+                )
+              ),
             );
           },
         ),
-      ),
+      )
     );
   }
 }
@@ -44,7 +64,7 @@ class HomeScreen extends StatelessWidget {
 class LinkItem {
   final String title;
   final String description;
-  final String icon;
+  final IconData icon;
   final int index;
 
   LinkItem({this.title,this.description,this.icon, this.index});
--- flutters/lib/MainScreen/index.dart
+++ flutters/lib/MainScreen/index.dart
@@ -4,9 +4,7 @@ import 'bottomNavi.dart';
 class MainScreen extends StatelessWidget {
   @override 
   Widget build(BuildContext context){
-    return Scaffold(
-      body: BottomNaviController()
-    );
+    return BottomNaviController();
   }
 }
 
